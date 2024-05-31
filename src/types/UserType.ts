43c193f5581bab27d2de15ab0c7a6b8eae93e75b
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-06-01 00:43:18 +0800

    add UserType

---
+++ src/types/UserType.ts
@@ -0,0 +1,15 @@
+export interface User {
+  bananas: number;
+  lastDayPlayed: string;
+  longestStreak: number;
+  name: string;
+  stars: number;
+  subscribed: boolean;
+  uid: string;
+  rank: number;
+}
+
+export interface AppState {
+  users: Record<string, User>;
+  searchedUser: string | null;
+}
