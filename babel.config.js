cc1717302c37d19206f8be4b9593749475d85f2e
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-06-01 00:40:40 +0800

    add babel plugin

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,12 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
+      '@babel/plugin-transform-private-methods',
+      {
+        loose: true,
+      },
+    ],
+    [
       'module-resolver',
       {
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
