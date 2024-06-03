ff1b3ed2c9563d1ea9cacc1908a9022551823e70
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2024-06-03 09:57:19 +0700

    create new filter and label constants

---
+++ src/constants/Filter.ts
@@ -0,0 +1,4 @@
+export const enum SORT_ORDER {
+  ASC = 'ascending',
+  DESC = 'descending',
+}
---
+++ src/constants/LabelData.ts
@@ -0,0 +1,11 @@
+export const enum LABELS_DATA {
+  NAME = 'Name',
+  RANK = 'Rank',
+  NUMBER_BANANAS = 'Number of bananas',
+}
+
+export const LABEL_NAMES: LABELS_DATA[] = [
+  LABELS_DATA.NAME,
+  LABELS_DATA.RANK,
+  LABELS_DATA.NUMBER_BANANAS,
+];
